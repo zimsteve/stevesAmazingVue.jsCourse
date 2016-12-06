0fc44ab3475674c84e821625b4522a4a5db059bd
Steve Sharp <zimsteve@live.com>
2016-12-07 06:05:57 +1300

    sample order form working with in-line script

--- steveVue.js
+++ steveVue.js
@@ -1,6 +1,50 @@
-new Vue({
-	el: '#app',
+// Define a custom filter called "currency".
+Vue.filter('currency', function (value) {
+  return '$' + value.toFixed(2)
+})
+
+var demo = new Vue({
+  el: '#main',
   data: {
-  	title: 'Hello World!'
+    	// Define the model properties. The view will loop through the services array and genreate a li element for every one of its items.
+    services: [
+        	{
+        		name: 'Web Development',
+        		price: 450,
+        		active: true
+        	}, {
+        		name: 'Design',
+        		price: 300,
+        		active: false
+        	}, {
+        		name: 'Integration',
+        		price: 250,
+        		active: false
+        	}, {
+        		name: 'Training',
+        		price: 220,
+        		active: false
+        	}, {
+        		name: 'Teaching James Adulting',
+        		price: 300,
+        		active: false
+        	}
+    ]
+  },
+  methods: {
+    	toggleActive: function (s) {
+      s.active = !s.active
+    	},
+    	total: function () {
+        	var total = 0
+
+        	this.services.forEach(function (s) {
+        		if (s.active) {
+        			total += s.price
+        		}
+        	})
+
+    	   return total
+    }
   }
 })
