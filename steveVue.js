0c54756a12099b1cbc4e23a440363db4954ea103
Steve Sharp <zimsteve@live.com>
2016-12-07 07:31:08 +1300

    working but needs css

--- steveVue.js
+++ steveVue.js
@@ -1,12 +1,17 @@
-new Vue({
-	el: '#app',
-  data: {
-  	title: 'Hello World!'
-  },
-  methods: {
-    // took me ages to reaslise that a sexy => wouldn't blady work here!! haha
-    changeTitle: function (e) {
-      this.title = e.target.value
-    }
-  }
+// Creating a new Vue instance and pass in an options object.
+var demo = new Vue({
+	// A DOM element to mount our view model.
+	el: '#main',
+  // This is the model.
+	// Define properties and give them initial values.
+	data: {
+		active: 'home'
+	},
+	// Functions we will be using.
+	methods: {
+		makeActive: function(item){
+			// When a model is changed, the view will be automatically updated.
+			this.active = item
+		}
+	}
 })
