38112a2e1e9606f425d3a68279f7291d305ef9a8
Steve Sharp <zimsteve@live.com>
2016-12-07 07:22:47 +1300

    searching with steveVue.js

--- steveVue.js
+++ steveVue.js
@@ -1,50 +1,64 @@
-// Define a custom filter called "currency".
-Vue.filter('currency', function (value) {
-  return '$' + value.toFixed(2)
-})
-
 var demo = new Vue({
   el: '#main',
   data: {
-    	// Define the model properties. The view will loop through the services array and genreate a li element for every one of its items.
-    services: [
-        	{
-        		name: 'Web Development',
-        		price: 450,
-        		active: true
-        	}, {
-        		name: 'Design',
-        		price: 300,
-        		active: false
-        	}, {
-        		name: 'Integration',
-        		price: 250,
-        		active: false
-        	}, {
-        		name: 'Training',
-        		price: 220,
-        		active: false
-        	}, {
-        		name: 'Teaching James Adulting',
-        		price: 300,
-        		active: false
-        	}
+    searchString: '',
+
+        // The data model. These items would normally be requested via AJAX,
+        // but are hardcoded here for simplicity.
+
+    articles: [
+      {
+        'title': 'What You Need To Know About CSS Variables',
+        'url': 'http://tutorialzine.com/2016/03/what-you-need-to-know-about-css-variables/',
+        'image': 'http://cdn.tutorialzine.com/wp-content/uploads/2016/03/css-variables-100x100.jpg'
+      },
+      {
+        'title': 'Freebie: 4 Great Looking Pricing Tables',
+        'url': 'http://tutorialzine.com/2016/02/freebie-4-great-looking-pricing-tables/',
+        'image': 'http://cdn.tutorialzine.com/wp-content/uploads/2016/02/great-looking-pricing-tables-100x100.jpg'
+      },
+      {
+        'title': '20 Interesting JavaScript and CSS Libraries for February 2016',
+        'url': 'http://tutorialzine.com/2016/02/20-interesting-javascript-and-css-libraries-for-february-2016/',
+        'image': 'http://cdn.tutorialzine.com/wp-content/uploads/2016/02/interesting-resources-february-100x100.jpg'
+      },
+      {
+        'title': 'Quick Tip: The Easiest Way To Make Responsive Headers',
+        'url': 'http://tutorialzine.com/2016/02/quick-tip-easiest-way-to-make-responsive-headers/',
+        'image': 'http://cdn.tutorialzine.com/wp-content/uploads/2016/02/quick-tip-responsive-headers-100x100.png'
+      },
+      {
+        'title': 'Learn SQL In 20 Minutes',
+        'url': 'http://tutorialzine.com/2016/01/learn-sql-in-20-minutes/',
+        'image': 'http://cdn.tutorialzine.com/wp-content/uploads/2016/01/learn-sql-20-minutes-100x100.png'
+      },
+      {
+        'title': 'Creating Your First Desktop App With HTML, JS and Electron',
+        'url': 'http://tutorialzine.com/2015/12/creating-your-first-desktop-app-with-html-js-and-electron/',
+        'image': 'http://cdn.tutorialzine.com/wp-content/uploads/2015/12/creating-your-first-desktop-app-with-electron-100x100.png'
+      }
     ]
   },
-  methods: {
-    	toggleActive: function (s) {
-      s.active = !s.active
-    	},
-    	total: function () {
-        	var total = 0
+  computed: {
+        // A computed property that holds only those articles that match the searchString.
+    filteredArticles: function () {
+      var articles_array = this.articles,
+        searchString = this.searchString
 
-        	this.services.forEach(function (s) {
-        		if (s.active) {
-        			total += s.price
-        		}
-        	})
+      if (!searchString) {
+        return articles_array
+      }
 
-    	   return total
+      searchString = searchString.trim().toLowerCase()
+
+      articles_array = articles_array.filter(function (item) {
+        if (item.title.toLowerCase().indexOf(searchString) !== -1) {
+          return item
+        }
+      })
+
+            // Return an array with the filtered data.
+      return articles_array
     }
   }
 })
